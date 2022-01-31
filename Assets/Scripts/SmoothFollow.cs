098563f036513d6c776864347cca9408801110ac
Isaac Vanier <ivanier31@gmail.com>
2022-01-30 23:28:38 -0600

    Kaiju proximity camera pan upward

--- Assets/Scripts/SmoothFollow.cs
+++ Assets/Scripts/SmoothFollow.cs
@@ -13,16 +13,49 @@ public class SmoothFollow : MonoBehaviour {
     public float heightDamping = 2.0f;
     public float rotationDamping = 3.0f;
 
+
+    public float minKaijuDistance;
+    public float maxKaijuDistance;
+    float yOffset = 0.0f;
+
+    float farYOffset = 0.0f;
+    float farHeight = 0.0f;
+    public float nearYOffset;
+    public float nearHeight;
+
+    public GameObject kaijuGo;
+    float kaijuDistance;
+
+
     // Place the script in the Camera-Control group in the component menu
     [AddComponentMenu("Camera-Control/Smooth Follow")]
 
+
+    // Place the script in the Camera-Control group in the component menu
+    private void Start() {
+
+        farHeight = height;
+        
+    }
+
+    private void Update() {
+
+        Vector3 direction = kaijuGo.transform.position - transform.position;
+        direction.y = 0.0f;
+
+        kaijuDistance = direction.magnitude;
+    }
+
     void LateUpdate() {
         // Early out if we don't have a target
-        if (!target) return;
+        if (!target)
+            return;
 
+        PanUp();
+        
         // Calculate the current rotation angles
         float wantedRotationAngle = target.eulerAngles.y;
-        float wantedHeight = target.position.y + height;
+        float wantedHeight = target.position.y + yOffset + height;
 
         float currentRotationAngle = transform.eulerAngles.y;
         float currentHeight = transform.position.y;
@@ -38,7 +71,12 @@ public class SmoothFollow : MonoBehaviour {
 
         // Set the position of the camera on the x-z plane to:
         // distance meters behind the target
-        transform.position = target.position;
+        //transform.position.Set(target.position.x, target.position.y, target.position.z);// = target.position;
+
+        Vector3 tempPos = target.position;
+        tempPos.y += yOffset;
+
+        transform.position = tempPos;
         transform.position -= currentRotation * Vector3.forward * distance;
 
         // Set the height of the camera
@@ -46,5 +84,19 @@ public class SmoothFollow : MonoBehaviour {
 
         // Always look at the target
         transform.LookAt(target);
+    }
+
+    // Look upward when near Kaiju.
+    void PanUp() {
+
+        if (kaijuDistance > minKaijuDistance) {
+            height = farHeight;
+            yOffset = 0.0f;
+
+            return;
+        }
+
+        height = Mathf.Lerp(nearHeight, farHeight, kaijuDistance / minKaijuDistance);
+        yOffset = Mathf.Lerp(nearYOffset, farYOffset, kaijuDistance / minKaijuDistance);
     }
 }
