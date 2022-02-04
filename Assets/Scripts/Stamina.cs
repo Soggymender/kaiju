e6b4babcaa0d74645b4fe5ae942131b8bd3d385a
Isaac Vanier <ivanier31@gmail.com>
2022-02-03 23:30:13 -0600

    Stamina gauge donut mask

--- Assets/Scripts/Stamina.cs
+++ Assets/Scripts/Stamina.cs
@@ -5,18 +5,28 @@ using UnityEngine.UI;
 
 public class Stamina : MonoBehaviour {
 
+    public Image mask;
     public Image image;
 
     bool discharge = false;
     float depletionLength = 4.0f;
     float rechargeLength = 2.0f;
 
+    // Hide if full for > this length of time. Inactive.
+    float fullLength = 2.0f;
+    float fullTime = 0.0f;
+
     [Range(0, 1)]
     public float value = 0;
 
     void Update() {
 
+        bool wantVisible = false;
+
         if (discharge) {
+
+            wantVisible = true;
+
             // Deplete
             if (value > 0.0f) {
 
@@ -31,24 +41,30 @@ public class Stamina : MonoBehaviour {
             // Recharge
             if (value < 1.0f) {
 
+                wantVisible = true;
+
                 value += Time.deltaTime / rechargeLength;
                 if (value >= 1.0f) {
                     value = 1.0f;
+                    fullTime = 0.0f;
                 }
+            }
+            else {
+                fullTime += Time.deltaTime;
+                if (fullTime < fullLength)
+                    wantVisible = true;
             }
         }
 
         image.fillAmount = value;
-    }
 
-    public void Show(bool show) {
-        // Leave THIS object active for recharging but hide the image.
-        image.gameObject.SetActive(show);
+        mask.gameObject.SetActive(wantVisible);
     }
 
     public void SetDischarge(bool discharge) {
 
         this.discharge = discharge;
+        
     }
 
     public float GetValue() {
